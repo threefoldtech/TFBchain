36d0ed8d6219c78425b6219e902dbffd6bc4a229
Glen De Cauwsemaecker <contact@glendc.com>
2019-09-06 09:50:37 +0200

    add files to ignore during blockchain generation

--- generate.sh
+++ generate.sh
@@ -3,8 +3,5 @@
 # generate blockchain
 rivinecg generate blockchain
 
-# keep our frontend icon packet
-git checkout frontend/explorer/public/assets/icon.png frontend/explorer/public/assets/icon.ico
-
 # update the vendor deps
 dep ensure -v -update
